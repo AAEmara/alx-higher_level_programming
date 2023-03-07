a952c2041d5c0c874f5cf0fa304aaa5a86e2b7f1
emara <abdelrahmanemara18@gmail.com>
2023-03-07 12:45:04 -0800

    Task1 modif: added the sign to lastdigit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastd = int(str(number)[-1])
+lastd = int(str(number)[-1]) * int((number/abs(number)))
 if lastd > 5:
     print(f"Last digit of {number} is {lastd} and is greater than 5")
 elif lastd == 0:
