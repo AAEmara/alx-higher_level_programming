35c3dc866304b543e46b1090c28b4cfac0689a01
emara <abdelrahmanemara18@gmail.com>
2023-03-10 08:41:43 -0800

    Add: Task2 source code

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    #Number of arguments without filename.
+    argc = (len(argv) - 1)
+    
+    if argc == 1:
+        print("{} argument:".format(argc))
+    elif argc > 1:
+        print("{} arguments:".format(argc))
+    else:
+        print("{} arguments.".format(argc))
+
+    if argc >= 1:
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, argv[i]))
