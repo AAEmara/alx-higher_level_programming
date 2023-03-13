0fc22b81b9e3fb6ff004042d921817a77b14d05b
emara <abdelrahmanemara18@gmail.com>
2023-03-13 03:45:06 -0700

    Add: Task4 soure code

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    cpd_list = my_list.copy()
+    if idx < 0 or idx >= len(my_list):
+        return cpd_list
+    else:
+        cpd_list[idx] = element
+        return cpd_list
