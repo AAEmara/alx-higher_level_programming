1bfa811b4484c49ddc91cc0b2ee97c8c7e02ceb8
emara <abdelrahmanemara18@gmail.com>
2023-03-13 16:10:42 -0700

    Add: Task8 source code

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        frst_char = None,
+    else:
+        frst_char = sentence[0][0]
+    rtrn_tuple = len(sentence), frst_char
+    return rtrn_tuple[0], rtrn_tuple[1]
