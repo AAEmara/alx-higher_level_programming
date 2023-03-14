49f101ae62fc54f3153e4dbe161c897b8f0bf136
emara <abdelrahmanemara18@gmail.com>
2023-03-13 17:07:13 -0700

    Task8 modif: returned None not as a tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        frst_char = None,
+        frst_char = None
     else:
         frst_char = sentence[0][0]
     rtrn_tuple = len(sentence), frst_char
