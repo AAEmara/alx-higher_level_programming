5757becce1faa81dc05f0d7d615691ef1819a5a9
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-08-14 14:43:39 -0700

    0x03: Task4 Python Script

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    list_cpy = my_list.copy()
+    max_id = len(my_list) - 1
+
+    if idx < 0:
+        return my_list
+    elif idx > max_id:
+        return my_list
+    else:
+        list_cpy[idx] = element
+        return list_cpy
