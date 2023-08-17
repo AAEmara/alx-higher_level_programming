e34b0c060e772af9864a798c0546c7150c94e287
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-08-16 18:22:35 -0700

    0x04: Task6 Python Script

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    keys = set(a_dictionary)
+    sorted_keys = sorted(a_dictionary)
+
+    for key in sorted_keys:
+        print("{:s}: {}".format(key, a_dictionary[key]))
