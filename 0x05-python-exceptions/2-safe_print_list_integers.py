29018902a3be2d1eee5b7452b2a50be70a122f43
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-08-28 10:57:51 -0700

    0x05: Task2 Python Script

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        list_cpy = my_list.copy()
+        list_cpy = list_cpy[: x]
+        count = 0
+
+    except AttributeError:
+        print("Please enter a list.")
+        return (count)
+
+    except TypeError:
+        print("Please check the number of values entered to print.")
+        return (count)
+
+    for i in range(0, x):
+        try:
+            print("{:d}".format(list_cpy[i]), end='')
+
+        except IndexError:
+            print("\nPlease check the number of values entered to print.")
+            return (count)
+
+        except (ValueError, TypeError):
+            pass
+
+        else:
+            count += 1
+
+    print()
+    return(count)
