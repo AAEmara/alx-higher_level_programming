b002b381093aa85ef48dd6c9611b0beedc20ebbb
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-08-28 10:32:03 -0700

    0x05: Task1 Python Script 1st Fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,11 +3,8 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
 
-    except ValueError:
-        print("Input is not an integer value.")
-
-    except TypeError:
-        print("Please check the datatype entered.")
+    except (ValueError, TypeError):
+        pass
 
     else:
         return (True)
