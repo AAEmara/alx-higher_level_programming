148696ff8c62ac31ad3da46f4856dd1352420698
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-09-02 15:51:03 -0700

    0x06: Task6 Python Script

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python3
+"""Square class documentation"""
+
+
+class Square(object):
+    """A Square class that aims to create a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializing the class with the size of a square.
+
+        Args:
+            size (:obj:`int`, 0): represents the size of the square.
+            position (:obj:`tuple` of :obj:`int`, (0,0)): represents the
+                                                    position in a square.
+
+        """
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """Calculates the area of a square and returns it.
+
+        Returns:
+            Area of the square.
+
+        """
+        area = self.__size * self.__size
+        return area
+
+    def my_print(self):
+        """Prints a square according to the size given using '#' character,
+        and the position given with space character.
+
+        Note:
+            If the size given is equal to ZERO a newline will be printed.
+            If position[1] is bigger than ZERO a newline will be printed
+            by the number of times given in position[1].
+
+        """
+
+        position = self.__position
+        size = self.__size
+
+        if position[1] > 0:
+            for line in range(0, position[1]):
+                print()
+
+        for i in range(0, size):
+            print(" " * position[0], end="")
+            for j in range(0, size):
+                print("#", end="")
+            print()
+
+        if size == 0:
+            print()
+
+    @property
+    def size(self):
+        """Getter function int: Returns the size value of the square.
+
+        Setter function: Changes the size of the square according to the
+        given value by the user.
+
+            Args:
+                value (int): value to be set as a square's size by the user.
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Checks the type of the given argument against integer type,
+        and also checks that the given value is not negative.
+
+        Note:
+            if any of the checks succeeded it raises an exception to the user.
+
+            else, it sets the given value by the user to the class attribute.
+
+        """
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        """Getter function :obj:`tuple` of :obj:`int`: Returns the position
+                                            inside a square.
+
+        Setter function: Sets the position inside the square as given by user.
+
+            Args:
+                value :obj:`tuple` of :obj:`int`: A tuple that contains two
+                integer values that represents the position.
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Checks the type of the value and the number of items inside it
+        before setting the position inside the square.
+
+            Note:
+                if the type of the value given isn't a tuple
+                OR
+                if the number of the items inside the tuple isn't 2,
+                an exception is raised.
+
+        """
+        if (type(value) != tuple) or (len(value) != 2):
+            raise TypeError('position must be a tuple of 2 positive integers')
+
+        self.__position = value
