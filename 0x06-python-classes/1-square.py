60cf6e04377c67e06d7802396b53cf72b0e93c9b
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-09-02 01:13:17 -0700

    0x06: Task1 Python Scipt 1st Fix
    Fixing the documentation of the __init__
    magic function.

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,10 +6,10 @@ class Square(object):
     """A Square class that aims to create a square."""
 
     def __init__(self, size=None):
+        """Initializing the class with the size of a square.
+
+        Args:
+            size (optional): represents the size of the sqaure.
+
+        """
         self.__size = size
-
-    """Initializing the class with the size of a square.
-
-    Args:
-        size (optional): represents the size of the sqaure.
-    """
