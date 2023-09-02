b9b9e3e98f6ddaa0dffe6d3164bc5bb2e78a150b
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-09-02 00:48:02 -0700

    0x06: Task1 Python Script

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square class documentation"""
+
+
+class Square(object):
+    """A Square class that aims to create a square."""
+
+    def __init__(self, size=None):
+        self.__size = size
+
+    """Initializing the class with the size of a square.
+
+    Args:
+        size (optional): represents the size of the sqaure.
+    """
