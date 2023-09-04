ca1549f617957a0e5c3ce63e9964fba6fcd46507
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-09-04 08:32:37 -0700

    0x08: Task0 Python Script, and README file

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Rectangle class documentation"""
+
+
+class Rectangle:
+    """An empty Rectangle class."""
+
+    pass
+    """Initializing the class with an empty rectangle."""
