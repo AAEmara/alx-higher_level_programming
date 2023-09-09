f7c587fb78a21d8f30137097309d36a688b17241
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-09-09 14:29:49 -0700

    0x07: Task0 Python Script, test cases, and README

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""0-add_integer module"""
+
+def add_integer(a, b=98):
+    """Adds two integers or floats.
+
+    Returns:
+        Integer sum of given values.
+    """
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    elif type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return (int(a) + int(b))
+
+if __name__ == "__main__":
+    import doctest
