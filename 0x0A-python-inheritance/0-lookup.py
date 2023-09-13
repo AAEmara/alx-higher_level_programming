d7f762fead9aaf2ae4afc57d53c0b3a5dff3c3a4
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-09-13 11:18:19 -0700

    0x0A: Task0 Python Script, and README File

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Lookup Module"""
+
+
+def lookup(obj):
+    """Looks up the methods and attributes of an object
+
+    Args:
+        obj: The object to have its methods and attributes returned.
+
+    Returns:
+        list of methods and attributes
+    """
+
+    att_methods_list = list(obj.__dict__)
+    return (att_methods_list)
