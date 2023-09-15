9131812d907a06fd0f25498b72773248d7d992e0
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-09-15 09:37:53 -0700

    0x07: Task4 Python Script, and test cases text

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""5-text_indentation module"""
+
+
+def text_indentation(text):
+    """Prints the text given with 2 new lines after '.', '?', and ':'
+    characters.
+
+    Raises:
+        TypeError if text input isn't a string datatype.
+
+    Args:
+        text (string): String input to be formatted before printing the output.
+
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for i in range(len(text)):
+        if i == 0 and text[i] == " ":
+            pass
+        elif i == len(text) - 1 and text[i] == " ":
+            pass
+        elif text[i] == " " and (text[i + 1] == " " or text[i - 1] in ".?:"):
+            pass
+        elif text[i] in ['.', '?', ':']:
+            print(f"{text[i]}\n")
+        else:
+            print(text[i], end="")
