f8e8c40d1bff1250f06d57b0e053093d3f3bab8a
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-09-16 06:41:37 -0700

    0x07: Task5 Python Script (Unittest)

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""6-max_integer_test Unittest module for max_integr(list=[]) function"""
+
+
+import unittest
+max_integer = __import__("6-max_integer").max_integer
+
+
+class test_max_integer(unittest.TestCase):
+    """Tests the max_integer(list=[]) function"""
+
+    def test_max(self):
+        """Tests the right output of the max_integer function"""
+
+        result_1 = max_integer([1, 2, 3])
+        self.assertEqual(result_1, 3)
+
+        result_2 = max_integer([-1, -2, -3])
+        self.assertEqual(result_2, -1)
+
+        result_3 = max_integer([-1, -2, -3, 0])
+        self.assertEqual(result_3, 0)
+
+    def test_input(self):
+        """Tests the input of the max_integer function"""
+
+        result_1 = max_integer()
+        self.assertEqual(result_1, None)
