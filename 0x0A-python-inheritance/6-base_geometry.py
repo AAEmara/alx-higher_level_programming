ea726b24dd0d53d24328bd6f4f33c88345a30c3c
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-09-17 04:37:46 -0700

    0x0A: Task6 Python Script

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""6-base_geometry module"""
+
+
+class BaseGeometry:
+
+    """BaseGeometry class."""
+    def area(self):
+
+        """Raises an exception because there is no implementation."""
+
+        raise Exception("area() is not implemented")
