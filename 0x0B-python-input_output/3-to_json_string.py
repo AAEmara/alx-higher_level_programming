8a8994f5529eb5bd038a33e4f173179c8154d0a7
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-09-25 13:45:28 -0700

    0x0B: Task3 Python Script

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""3-to_json_string module."""
+
+
+import json
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object.
+
+    Returns:
+        String object through using JSON representation.
+
+    Args:
+        my_obj (optional): Object to be represented as in JSON as a string.
+    """
+    str_obj = json.dumps(my_obj)
+
+    return (str_obj)
