50f2985e7ec6b4dfc525b4b51550ed9fd27d4666
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-09-25 05:40:22 -0700

    0x0B: Task0 Python Script 1st Fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,7 +8,6 @@ def read_file(file_name=""):
 
     Args:
         file_name (string): the relative path to the file or the filename.
- 
     """
     with open(file_name, mode='r', encoding='utf-8') as r_file:
-        print(r_file.read())
+        print(r_file.read(), end="")
