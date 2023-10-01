bb7365f0b9bbe930e97a3c5a56125684c7fe46e9
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-10-01 15:05:31 -0700

    0x0B: Task6 Python Script

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""6-load_from_json_file module"""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an object from a JSON file.
+
+    Args:
+        filename (string): path to file or filename.
+    """
+    with open(filename, mode="r", encoding="utf-8") as r_file:
+        file_str = r_file.read()
+
+    return(json.loads(file_str))
