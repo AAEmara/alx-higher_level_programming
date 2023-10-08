98caa1def2bdd4f24f85d37bdb5b1d17ac19bf7e
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-10-08 00:49:51 -0700

    0x0B: Task7 Python Script 3rd Fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,12 @@ import sys
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
-try:
-    obj = load_from_json_file("add_item.json")
-except (FileNotFoundError, json.decoder.JSONDecodeError):
-    obj = []
+if __name__ == "__main__":
 
-obj.extend(sys.argv[1:])
-save_to_json_file(obj, "add_item.json")
+    try:
+        obj = load_from_json_file("add_item.json")
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        obj = []
+
+    obj.extend(sys.argv[1:])
+    save_to_json_file(obj, "add_item.json")
