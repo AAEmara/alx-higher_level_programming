83e0639940550ed932448c87b327df5e89dcc854
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-10-08 00:45:09 -0700

    0x0B: Task7 Python Script 2nd Fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""7-add_item module, add arguments to a json file."""
+"""7-add_item module add arguments to a python list and saved to a json file"""
 import sys
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
