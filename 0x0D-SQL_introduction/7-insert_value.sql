3cd3bfb0e37c8e4258bf788c70ea17e0076db748
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-10-17 10:38:58 -0700

    0x0D: Task7 SQL Script.

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+-- Updating the "first_table" table in "hbtn_0c_0" database.
+-- By inserting a new row in the table.
+INSERT INTO first_table VALUES (
+        89,
+        "Best School"
+        )
