63f2415d124da47658d25a2639ef804ddfa61c63
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-10-17 14:05:01 -0700

    0x0D: Task9 SQL Script.

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- Creating a "second_table" table in "hbtn_0c_0" database.
+-- Updating the table with multiple rows.
+CREATE TABLE IF NOT EXISTS second_table (
+        id INT,
+        name VARCHAR(256),
+        score INT
+);
+INSERT INTO second_table 
+VALUES  (1, "John", 10),
+        (2, "Alex", 3),
+        (3, "Bob", 14),
+        (4, "George", 8);
