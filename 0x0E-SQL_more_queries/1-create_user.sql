afa1c8d20fe1e326496bc6104989a874bf412ac4
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-10-18 02:58:23 -0700

    0x0E: Task1 SQL Script 1st Fix.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,7 @@
 -- Creating a user name "user_0d_1" for MySQL server.
 CREATE USER IF NOT EXISTS "user_0d_1"@"localhost"
-IDENTIFIED BY 'user_0d_1_pwd';
+IDENTIFIED BY "user_0d_1_pwd";
+
+GRANT ALL
+ON *.*
+TO "user_0d_1"@"localhost"
