05320be802c5105ce0deddce8b0d8d087313ef4b
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-11-03 10:35:00 -0700

    0x0C: Task11 Updating Python Script.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,3 +11,17 @@ class Square(Rectangle):
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """int: Integer value of the square's side length."""
+        return (self.width)
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("width must be an integer")
+        if size <= 0:
+            raise ValueError("width must be > 0")
+        self.width = size
+        self.height = size
