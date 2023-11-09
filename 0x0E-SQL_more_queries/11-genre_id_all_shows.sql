054382b4b6a84936015e73c52aa1f8ef0d0395e4
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-11-09 04:13:07 -0800

    0x0E: Task11 SQL Script 1st Fix.

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- Reading records for titles and genre id, which are sorted
 -- in ascending order. If a show doesn't have a genre, display NULL.
-SELECT tv_shows.title as title, tv_show_genres.genre_id as genre_id
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
