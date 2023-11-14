6f5722a0029aa47630dbb5e10f7a6de95583e688
AAEmara <abdelrahmanemara18@gmail.com>
2023-11-14 00:53:08 -0800

    0x12: Task3 JavaScript 1st Fix.

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv.length === 2) {
-  console.log('No argument');
-} else {
-  let i = 2;
+let i = 0;
+while (argv[i]) {
+  i++;
+}
+if (i === 2) {
+  console.log('No Argument');
+}
+else {
+  i = 2;
   while (argv[i]) {
     console.log(argv[i]);
     i++;
