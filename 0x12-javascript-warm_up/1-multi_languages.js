98c8752baa158ab518b22535ed053709a054efb7
AAEmara <abdelrahmanemara18@gmail.com>
2023-11-13 17:40:19 -0800

    0x12: Task1 JavaScript 1st Fix.

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const Clang = 'C is fun';
 const Pylang = 'Python is cool';
-const Jslang = 'Javascript is amazing';
+const Jslang = 'JavaScript is amazing';
 console.log(Clang + '\n' + Pylang + '\n' + Jslang);
