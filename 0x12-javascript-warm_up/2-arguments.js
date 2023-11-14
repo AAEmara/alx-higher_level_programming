42d928923c2bcde463a23914e4e4e4c3f54b2577
AAEmara <abdelrahmanemara18@gmail.com>
2023-11-14 00:17:40 -0800

    0x12: Task2 JavaScript Script 2nd Fix.

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-const arg_len = argv.length
-if (arg_len === 2) {
+if (argv.length === 2) {
   console.log('No argument');
-} else if (arg_len === 3) {
+} else if (argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
