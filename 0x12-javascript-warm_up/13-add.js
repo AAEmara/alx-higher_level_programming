b7e15d2274c1921ceb37593edc154859b0c0cc9f
AAEmara <abdelrahmanemara18@gmail.com>
2023-11-15 05:33:44 -0800

    0x12: Task13 JavaScript Script 1st Fix.

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-const args = process.argv;
-const Arg1 = parseInt(args[2]);
-const Arg2 = parseInt(args[3]);
 exports.add = function add (a, b) {
   const sum = parseInt(a) + parseInt(b);
   return (sum);
-}
+};
