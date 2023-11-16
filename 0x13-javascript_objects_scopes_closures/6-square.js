71cf57f6168611f54e4672f407df3ce5c9f719d9
AAEmara <abdelrahmanemara18@gmail.com>
2023-11-15 23:52:47 -0800

    0x13: Task6 JavaScript Script 1st Fix.

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,15 +3,14 @@ const Square2 = require('./5-square.js');
 module.exports = class Square extends Square2 {
   constructor (size) {
     super(size, size);
-    this.num = size;
   }
 
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.num; i++) {
-      console.log(c.repeat(this.num));
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 };
