78d840f9f759944fb29e92ff2d3dcc05b2636bd1
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-11-18 13:31:48 -0800

    0x0F: Task1 Python Script 1st Fix.
    Making sure to take into consideration
    the case-senstive substring matches through
    using the LIKE BINARY SQL Statement.

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states\
-                 WHERE states.name LIKE \"N%\"\
+                 WHERE states.name LIKE BINARY \"N%\"\
                  ORDER BY states.id ASC;")
     rows = cur.fetchall()
     for row in rows:
