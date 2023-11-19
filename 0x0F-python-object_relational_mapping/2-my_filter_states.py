dc9948aaf9cc642beef7d0610f3ead31a8b94dea
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-11-18 23:39:32 -0800

    0x0F: Task2 Python Script 1st Fix.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,9 +9,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=args[1], passwd=args[2], db=args[3])
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states\
-                 WHERE states.name = %s\
-                 ORDER BY states.id ASC;", (args[4],))
+    query = "SELECT * FROM states\
+                 WHERE states.name = '{}'\
+                 ORDER BY states.id ASC;".format(args[4])
+    cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
         print(f"({row[0]}, '{row[1]}')")
