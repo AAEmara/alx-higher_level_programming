284e9fbd7020f5ed4422814d7d1ae35cedac3388
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-11-19 01:12:09 -0800

    0x0F: Task5 Python Script.

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A module for connecting and executing a select statement in MySQLdb."""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    args = sys.argv
+    db = MySQLdb.connect(user=args[1], passwd=args[2], db=args[3])
+    cur = db.cursor()
+
+    query = "SELECT c.name\
+             FROM cities AS c\
+             JOIN states AS s\
+             ON c.state_id = s.id\
+             WHERE s.name = %s\
+             ORDER BY c.id ASC;"
+    cur.execute(query, (args[4],))
+    rows = cur.fetchall()
+    for i, row in enumerate(rows):
+        if (i < (len(rows) - 1)):
+            print(row[0], end=', ')
+        else:
+            print(row[0], end='\n')
