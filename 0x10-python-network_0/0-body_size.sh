5ddca54a7bc839b0507b04ea2dfb4db413eb4d10
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-11-30 05:32:14 -0800

    0x10: Task0 Bash Script 1st Fix.

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes a URL, and sends a request to the URL.
-# Displays the size of the body of the response.
+# Takes a URL, and sends a request to the URL. Displays the size of the body of the response.
 curl -so /dev/null "$1" -w "%{size_download}\n"
