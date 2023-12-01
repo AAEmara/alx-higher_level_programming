4aad6b1172c8758310f5ead384e16a9df9a41bed
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2023-11-30 18:08:39 -0800

    0x10: Task6 Python Script.

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for find_peak function."""
+
+
+def find_peak(nums):
+    """A function that takes a list and returns the highest number."""
+    count = len(nums)
+    if count == 0:
+        return (None)
+    else:
+        for i in range(1, count):
+            if nums[i] >= nums[i - 1] and nums[i] >= nums[i + 1]:
+                return (nums[i])
+            elif nums[i - 1] >= nums[i + 1] and i - 1 == 0:
+                return (nums[i - 1])
+            elif nums[i - 1] <= nums[i + 1] and i + 1 == count:
+                return (nums[i + 1])
