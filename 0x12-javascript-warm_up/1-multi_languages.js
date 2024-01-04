1b0317149caef6e46882c0a7491ec394e0cda364
Abdelrahman Emara <abdelrahmanemara18@gmail.com>
2024-01-04 21:40:32 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const Clang = 'C is fun';
-const Pylang = 'Python is cool';
-const Jslang = 'JavaScript is amazing';
-console.log(Clang + '\n' + Pylang + '\n' + Jslang);
--- 0x12-javascript-warm_up/2-arguments.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const ArgLen = process.argv.length - 2;
-if (ArgLen === 0) {
-  console.log('No argument');
-} else if (ArgLen === 1) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
