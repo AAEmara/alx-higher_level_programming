e162ed05830032c40bcc1568af0cb4029b3ef71c
AAEmara <abdelrahmanemara18@gmail.com>
2024-01-14 14:57:18 -0800

    0x0F: Task7 Python Script - 1st Fix.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,12 +5,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-url = f'mysql+mysqldb:\
+if __name__ == "__main__":
+    url = f'mysql+mysqldb:\
 //{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}'
-engine = create_engine(url)
-Session = sessionmaker(bind=engine)
-Session.configure(bind=engine)
-session = Session()
-instances = session.query(State).order_by(State.id)
-for instance in instances:
-    print(f'{instance.id}: {instance.name}')
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+    Session.configure(bind=engine)
+    session = Session()
+    instances = session.query(State).order_by(State.id)
+    for instance in instances:
+        print(f'{instance.id}: {instance.name}')
