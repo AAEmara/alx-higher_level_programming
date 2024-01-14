9242deee161064de8f27487d94b5ef169fdbfabd
AAEmara <abdelrahmanemara18@gmail.com>
2024-01-14 12:41:06 -0800

    0x0F: Task6 Python Script.

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""Module model_state that defines State class."""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State (Base):
+    """State Class defined based on Base Class."""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
