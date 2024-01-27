e73d6a5957cfa8da5bb7573d7847b421f69ac064
AAEmara <abdelrahmanemara18@gmail.com>
2024-01-26 18:12:29 -0800

    0x11: Task8 Python Script. (requests library)

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Takes in a letter and sends a `POST` request to a certain URL with
+the letter."""
+
+
+import requests
+import sys
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if (len(sys.argv) == 1):
+        letter = ""
+    else:
+        letter = sys.argv[1]
+
+    r = requests.post(url, data={"q": letter})
+    r = r.json()
+    if (not r):
+        print("No result")
+    else:
+        print(f"[{r['id']}] {r['name']}")
